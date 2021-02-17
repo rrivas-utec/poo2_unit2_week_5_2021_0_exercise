d0008853d87f2da26549e6ec53e0b565a7119598
rrivas-utec <rrivas@utec.edu.pe>
2021-02-17 17:02:49 -0500

    Initial commit

---
+++ p4.cpp
@@ -0,0 +1,10 @@
+//
+// Created by rudri on 10/11/2020.
+//
+
+#include <iostream>
+#include "p4.h"
+
+using namespace std;
+
+void pregunta_4() {}
---
+++ p5.h
@@ -0,0 +1,10 @@
+//
+// Created by rudri on 10/11/2020.
+//
+
+#ifndef poo2_unit2_week_5_2021_0_P5_H
+#define poo2_unit2_week_5_2021_0_P5_H
+
+void pregunta_5();
+
+#endif //poo2_unit2_week_5_2021_0_P5_H
